f5da6d419314fd404d27ba75dc0b5392c8a07beb
saeedjameii <saeed.jamei.amir@gmail.com>
2026-09-06 12:24:32 +0330

    refactor: remove HomeJwtCookieMiddleware and update home route

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,10 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\JwtCookieMiddleware;
-use App\Http\Middleware\HomeJwtCookieMiddleware;
 
 Route::get('/', function () {
     return view('home');
-})->middleware(HomeJwtCookieMiddleware::class)->name('home');
+})->name('home');
 
 Route::get('sign-up', [AuthController::class, 'signUp'])->name('signUp');
 Route::post('sign-up', [AuthController::class, 'signUpPost'])->name('signUp.post');
